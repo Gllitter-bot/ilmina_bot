943737b2a933f269a9e821873d50b1fa8d2884d3
ef366 <uw.shin.aa89@gmail.com>
2017-05-18 04:43:54 +0900

    !exitコマンドと他のコマンドが同時に実行されないように修正
    加えて細かい修正もしました。

--- ilmina.py
+++ ilmina.py
@@ -18,24 +18,26 @@ async def on_message(message):
     if message.author == client.user:
         return  # Botのメッセージには反応しない
 
-    elif message.content.startswith("イルミナ"):
-        await client.send_message(message.channel, "呼んだ？")
-
-    elif message.content.startswith("!exit"):
+    if message.content == "!exit":
         await client.send_message(message.channel, "さよなら～")
         await client.logout() # ログアウトとプログラムの終了
         print("終了しました")
 
-    f = {}
+    elif message.content.count("イルミナ"):
+        await client.send_message(message.channel, "呼んだ？")
+
+
+    dic = {}
     with codecs.open('template.txt', 'r', 'utf-8') as text:
         for line in text:
-            line = line.rstrip('\r\n') #改行の削除
-            fields = line.split('^')   #^で分割
-            f[fields[0]] = fields[1]   #ディクショナリに追加
-    keys = f.keys() #キーのリスト化
-    for value in keys:
-        if message.content.count(value):
-            await client.send_message(message.channel, 'どうぞ！\n{}'.format(f[value]))
+            line = line.rstrip('\r\n') # 改行の削除
+            fields = line.split('^')   # ^で分割
+            dic[fields[0]] = fields[1] # ディクショナリに追加
+        keys = dic.keys() # キーのリスト化
+        for value in keys: # keys内の要素の検証
+            if message.content.count(value):
+                await client.send_message(message.channel, 'どうぞ！\n{}'.format(dic[value]))
+                print("テンプレートを送信:", value)
 
 
 client.run('TOKEN_PHRASE')
